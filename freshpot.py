0d375c744966bb1105c9e0849b0de7a0f0401be1
JakeVdub <jakevandewalle@gmail.com>
2016-05-29 17:27:45 -0500

    Update

--- freshpot.py
+++ freshpot.py
@@ -1,6 +1,7 @@
 import time
 import RPi.GPIO as GPIO
 from twython import TwythonStreamer
+from twython import Twython
 
 GPIO.setmode(GPIO.BCM)
 
@@ -17,6 +18,11 @@ power_pin = 18 ## SSR Input pin
 GPIO.setup(power_pin, GPIO.OUT) ## Sets it up as an output
 GPIO.output(power_pin, False) ## Initially turned off
 
+## Sets up Twython for tweeting
+app = Twython(APP_KEY, APP_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET) 
+
+## Text for tweet
+tweet_text = "Fresh Pot is Ready!"
 
 ## Setup callbacks from Twython Streamer
 class FreshPotStreamer(TwythonStreamer):
@@ -27,8 +33,10 @@ class FreshPotStreamer(TwythonStreamer):
     time.sleep(360) ## Makes the coffee for 6 minutes, may need adjusting
     print "POWER OFF, FRESH POT!!"
     GPIO.output(power_pin, False) ## Turns coffee maker off
+    app.update_status(status = tweet_text) ##sets text for tweet
+    print "Tweeted Back At Ya!"
     time.sleep(300) ## Prevents it from being ran again for 5 minutes
-  
+    
 
 ## Create Streamer
 try:
