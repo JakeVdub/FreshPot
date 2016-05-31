ef0e135c783bc2339a5e3002430462d12fc63166
Jake VandeWalle <jakevandewalle@gmail.com>
2016-05-30 22:36:03 -0500

    Merged ip.py and freshpot.py

--- freshpot.py
+++ freshpot.py
@@ -2,6 +2,31 @@ import time
 import RPi.GPIO as GPIO
 from twython import TwythonStreamer
 from twython import Twython
+import time
+import Adafruit_CharLCD as LCD
+from Adafruit_CharLCD import Adafruit_CharLCD
+from subprocess import *
+from time import sleep, strftime
+from datetime import datetime
+
+
+
+## Raspberry Pi pin configuration:
+lcd_rs        = 27 
+lcd_en        = 22
+lcd_d4        = 25
+lcd_d5        = 24
+lcd_d6        = 23
+lcd_d7        = 13
+lcd_backlight = 4
+
+## Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows    = 2
+
+## Initialize the LCD using the pins above.
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
 
 GPIO.setmode(GPIO.BCM)
 
@@ -37,11 +62,42 @@ class FreshPotStreamer(TwythonStreamer):
     print "Tweeted Back At Ya!"
     time.sleep(300) ## Prevents it from being ran again for 5 minutes
     
+## IP Address
+class getIPAddress():
+  def get_ip_addr(self)
+    cmd = "ip addr show scope global wlan0 | grep inet | cut -d' ' -f6 | cut -d/ -f1"
+    output = ""
+    ## This loop keeps attempting to find the IP, if after one minute it does not work
+    ## then it returns an error output so the screen can display the error message
+    while (retry < 30) and (output == ""):
+      p = Popen(cmd, shell=True, stdout=PIPE)
+      output = p.communicate()[0] 
+      if output == "":  
+        time.sleep(3)
+        retry = retry + 1
+    if retry > 30:
+      output == "error"
+    return output
+    msg = output
+    
+    if msg == "error"
+      msg == "DISCONNECTED"
+    
+    lcd.message(msg)
+  
+  
+ipaddr = getIPAddress()
+ipaddr.get_ip_addr()
 
 ## Create Streamer
 try:
   stream = FreshPotStreamer(APP_KEY, APP_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
   stream.statuses.filter(track=TERMS)
+  
+
+  
+  
+  
 except KeyboardInterrupt:
   GPIO.cleanup()
 
